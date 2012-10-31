3f4cae814a76c3c2c071d577e5d98050ef69693e
vm18648 <vm18648@vm18648.(none)>
2012-10-30 21:01:05 -0400

    Now adding waitUntilNextPeriod system call. Missed it in last commit

---
+++ tegra/waitUntilNextPeriod/waitUntilNextPeriod.c
@@ -0,0 +1,41 @@
+/* my_syscall.c - Our own syscall */
+
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/time.h>
+#include <asm/uaccess.h>
+
+asmlinkage int sys_setProcessBudget(pid_t pid) {
+
+    struct task_struct * curr;
+   
+    if (pid <= 0) {
+	printk("Invalid PID\n");
+	return -EINVAL;
+    }
+
+    //Finding task struct given its pid
+    read_lock(&tasklist_lock);
+    curr = (struct task_struct *) find_task_by_vpid(pid);
+    read_unlock(&tasklist_lock);
+    if(curr == NULL){
+	printk("Couldn't find task\n");
+	return -ESRCH;
+    }
+
+    //Putting task to sleep
+    curr->state = TASK_UNINTERRUPTIBLE;
+    set_tsk_need_resched(curr);
+    
+    //Setting compute time to Budget time just 
+    //for safety in case it ever comes in run queue
+    //before time period , it will automatically be put
+    //to sleep
+    (curr->compute_time).tv_sec = (curr->budget_time).tv_sec;
+    (curr->compute_time).tv_nsec = (curr->budget_time).tv_nsec;
+
+    printk("Wait until next period %d\n",pid);
+
+    return 0;
+}
